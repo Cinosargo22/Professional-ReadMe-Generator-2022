00f48a19352f24ea9fe15425d3c1ab9388f9f821
Cinosargo22 <cinosargo22@gmail.com>
2022-08-20 01:11:00 -0700

    fourth commit

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,21 +1,30 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
 
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Licensing:
+  [![License](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)](https://opensource.org/licenses/Apache-2.0)
+  ## Table of Contents 
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contribution](#contribution)
+  - [Testing](#testing)
+  - [Additional Info](#additional-info)
+  ## Description:
+  ${data.description}
+  ## Installation:
+  ${data.installation}
+  ## Usage:
+  ${data.usage}
+  ## License:
+  ${data.licensing}
+  ## Contribution:
+  ${data.contribution}
+  ## Testing:
+  ${data.testing}
+  ## Additional Info:
+  - Github: [${data.github}](https://github.com/${data.github})
+  - Email: ${data.email} `;
 }
 
 module.exports = generateMarkdown;
