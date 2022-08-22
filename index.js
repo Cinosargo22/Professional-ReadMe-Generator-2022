0cba0ce372d6755a35c5c0dfbcedd2f55da02564
Cinosargo22 <cinosargo22@gmail.com>
2022-08-21 23:48:25 -0700

    sixth commit

--- index.js
+++ index.js
@@ -1,153 +1,47 @@
 // TODO: Include packages needed for this application
-// Global require's
+
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./src/generateMarkdown.js')
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
 
-    // Array of questions to ask the user
-const questions = [
-    // Project name
+    
+inquirer
+    .prompt([
     {
-        type: 'input',
-        name: 'title',
-        message: 'What is the title of the project? (Required)',
-        validate: titleInput => {
-            if (titleInput) {
-                return true;
-            } else {
-                console.log('You need to enter a title to continue.');
-                return false;
-            }
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username" 
+    },
+    {
+        type: "input",
+        message: "What is your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "What is the title of this project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Short project desciption",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "Choose a license for your project",
+        name: "license",
+        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'BSD', 'None'],
+    }
+]).then((data) => {
+    console.log(data);
+    fs.writeFile('README.md', generateMarkdown(data),
+    error =>{
+        if (error) {
+            console.log('Fill out all data')
         }
-    },
-    // Description of project
-    {
-        type: 'input',
-        name: 'description',
-        message: 'Provide a description of the project (Required)',
-        validate: descriptionInput => {
-            if (descriptionInput) {
-                return true;
-            } else {
-                console.log('Provide a project description.');
-                return false;
-            }
-        }
-    },
-    // Installation Instructions
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'How do you install your project? (Required)',
-        validate: installationInput => {
-            if (installationInput) {
-                return true;
-            } else {
-                console.log('Provide installation info to continue.');
-                return false;
-            }
-        }
-    },
-    // Usage Information
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'How do you use this project? (Required)',
-        validate: usageInput => {
-            if (usageInput) {
-                return true;
-            } else {
-                console.log('Provide information on how to use project.');
-                return false;
-            }
-        }
-    },
-    // Contribution Guidlines
-    {
-        type: 'input',
-        name: 'contribution',
-        message: 'How should people contribute to this project? (Required)',
-        validate: contributionInput => {
-            if (contributionInput) {
-                return true;
-            } else {
-                console.log('Provide information on how to contribute to the project.');
-                return false;
-            }
-        }
-    },
-    // Test Instructions 
-    {
-        type: 'input',
-        name: 'testing',
-        message: 'How do you test this project? (Required)',
-        validate: testingInput => {
-            if (testingInput) {
-                return true;
-            } else {
-                console.log('Describe how to test this project.');
-                return false;
-            }
-        }
-    },
-    // License Options
-    {
-        type: 'checkbox',
-        name: 'licensing',
-        message: 'Choose a license for your project (Required)',
-        choices: ['Apache', 'MIT', 'Mozilla-Public', 'GNU-General-Public', 'Common-Development-and Distribution', 'None'],
-        validate: licensingInput => {
-            if (licensingInput) {
-                return true;
-            } else {
-                console.log('Pick a license for the project.');
-                return false;
-            }
-        }
-    },
-    // Github Username
-    {
-        type: 'input',
-        name: 'github',
-        message: 'Enter your GitHub Username (Required)',
-        validate: githubInput => {
-            if (githubInput) {
-                return true;
-            } else {
-                console.log('Please enter your GitHub username.');
-                return false;
-            }
-        }
-    },
-    // Email Address
-    {
-        type: 'input',
-        name: 'email',
-        message: 'Would you like to include your email?',
-    },
-];
-
-// TODO: Create a function to write README file
-// Function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-        if (err)
-            throw err;
-        console.log('Success! Information transferred to the README!')
-    });
-};
-
-// TODO: Create a function to initialize app
-// Function to initialize app
-function init() {
-    inquirer.prompt(questions)
-    .then(function (userInput) {
-        console.log(userInput)
-        writeToFile("README.md", generateMarkdown(userInput));
-    });
-};
-
-// Function call to initialize app
-init();
+        console.log('README was generated')
+    }
+)})
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,27 @@
-
+function renderLicense(license) {
+  if (license === 'Apache') {
+  return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+}
+if (license === 'MIT') {
+  return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+}
+  if (license === 'Mozilla-Public') {
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  }
+  if (license === 'GNU-General-Public') {
+    return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)'
+  }
+  if (license === 'BSD') {
+    return '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+  }
+  if (license === 'None') {
+    return ''
+  }
+}
 function generateMarkdown(data) {
   return `# ${data.title}
-  ## Licensing:
-  [![License](https://img.shields.io/badge/License-Apache_2.0-yellow.svg)](https://opensource.org/licenses/Apache-2.0)
+
+  [![license](https://img.shields.io/badge/license-${data.licensing}-blue)](https://shields.io)
   ## Table of Contents 
   - [Description](#description)
   - [Installation](#installation)
